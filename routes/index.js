fde421afeaa4be1bffdb59332c49793caadb84d8
Gediminas Ubartas <gediminas@largecode.com>
2013-02-17 17:29:12 +0200

    Simple file upload

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'File upload' });
 };
+exports.uploaded = function(req, res){
+    res.render('uploaded', { title: 'File upload' });
+}
